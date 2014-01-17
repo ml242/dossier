67bc33cd19d2a18aac4e174a09403e7c0ddead59
Harrison Powers <fijimunkii@gmail.com>
2014-01-16 20:40:19 -0500

    fb initalizer setup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['ALBW_FACEBOOK_KEY'], ENV['ALBW_FACEBOOK_SECRET'], {:provider    _ignores_state => true}    
+  provider :facebook, ENV['DOSSIER_FACEBOOK_KEY'], ENV['DOSSIER_FACEBOOK_SECRET'], {:provider_ignores_state => true}    
 end 
+
 # OmniAuth error handling http://stackoverflow.com/a/11256549/2474735
 # OmniAuth.config.on_failure = UsersController.action(:oauth_failure)
