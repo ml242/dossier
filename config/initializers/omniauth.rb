e61b509a37cff3564d86c5cf7e3423ba3acc2201
Matt Lucas <mattlucas@gmail.com>
2014-01-20 16:02:35 -0500

    added env variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   APP_ID = ENV['DOSSIER_FACEBOOK_KEY']
   APP_SECRET = ENV['DOSSIER_FACEBOOK_SECRET']
   provider :facebook, APP_ID, APP_SECRET, {:provider_ignores_state => true}
+  Key = ENV['Meetup_ID']
+  Secret = Env['Meetup_Secret']
+  provider :meetup, Key, Secret
 end
 
 # OmniAuth error handling http://stackoverflow.com/a/11256549/2474735
