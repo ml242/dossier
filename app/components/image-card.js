b13b81d2e100c25d1ae58f62166e5b174bd20864
Harrison Powers <fijimunkii@gmail.com>
2014-10-16 15:29:16 -0400

    Fix imageText ref

--- app/components/image-card.js
+++ app/components/image-card.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   click: function() {
     console.log(this.$());
-    var image = this.$().find('.invisible')
-    image.toggleClass('invisible');   
+    var imageText = this.$().find('.image-text')
+    imageText.toggleClass('invisible');   
   }
 });
 
