66143f9d76ac8d1d0b9cb31900fe73a5f21a79a5
ml242 <mattlucas@gmail.com>
2014-10-16 15:09:54 -0400

    EMBER GETS IN THE WAY GAH

--- app/components/image-card.js
+++ app/components/image-card.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   click: function() {
-    console.log('not implemented');
+    console.log(this.$());
+    var image = this.$().find('.invisible')
+    image.toggleClass('invisible');   
   }
 });
+
