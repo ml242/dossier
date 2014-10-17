a2b25cf4422c4921533c233f2f6bb133c201a385
ml242 <mattlucas@gmail.com>
2014-10-17 17:21:52 -0400

    addressing the issues with the style on the main page after generating the tiled images

--- app/components/image-card.js
+++ app/components/image-card.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   click: function() {
     console.log(this.$());
-    var imageText = this.$().find('.image-text')
+    var imageText = this.$().find('.image-text');
     imageText.toggleClass('invisible');   
   }
 });
